f98c0887cb520134e1d42cac5c9e08d6f85c73ab
Huntears <dev@huntears.com>
2021-02-08 23:07:47 +0100

    Add calloc

--- src/calloc.c
+++ src/calloc.c
@@ -5,9 +5,15 @@
 ** calloc
 */
 
-#include "malloc.h"
+#include <string.h>
+
+#include "my_malloc.h"
 
 void *calloc(size_t nmemb, size_t size)
 {
-    return NULL;
+    size_t size_total = nmemb * size;
+    void *ptr = malloc(size_total);
+
+    memset(ptr, 0, size_total);
+    return ptr;
 }
