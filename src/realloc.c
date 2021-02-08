be6de5e50b4e6d980c7b9a53edf1ce691a5aec01
Huntears <dev@huntears.com>
2021-02-08 23:08:13 +0100

    Add realloc

--- src/realloc.c
+++ src/realloc.c
@@ -5,9 +5,24 @@
 ** realloc
 */
 
-#include "malloc.h"
+#include <string.h>
+
+#include "my_malloc.h"
 
 void *realloc(void *ptr, size_t size)
 {
-    return NULL;
+    void *new_ptr = NULL;
+    meta_t *block_ptr = NULL;
+
+    if (!ptr)
+        return malloc(size);
+    block_ptr = get_block_ptr(ptr);
+    if (block_ptr->size >= size)
+        return ptr;
+    new_ptr = malloc(size);
+    if (!new_ptr)
+        return NULL;
+    memcpy(new_ptr, ptr, block_ptr->size);
+    free(ptr);
+    return new_ptr;
 }
